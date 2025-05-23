2ddb3f56cbead6b1ea3968c73ea99d403dd46fcf
mdecycu <mdecycuorg@gmail.com>
2025-05-23 14:59:24 +0800

    add g7

--- init.py
+++ init.py
@@ -11,7 +11,8 @@ class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
     site_title = "cmsimde_site"
-    ip = "127.0.0.1"
+    #ip = "127.0.0.1"
+    ip = "2001:288:6004:17:2023:cba:0:100"
     dynamic_port = 9443
     static_port = 8443
     def __init__(self):
